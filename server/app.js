0ff9ea4dd8641224487cd28382253ccc8d0d9585
Nicolas Sam <samiam151@gmail.com>
2019-05-15 13:41:51 -0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,6 +7,7 @@ const compression = require("compression");
 const PORT = process.env.PORT || 5105;
 const RedisStore = require("connect-redis")(session);
 const enforce = require("express-sslify");
+const cluster - requre("cluster");
 
 const UserService = require("./services/userService");
 const RssFeedService = require("./services/rssFeedService");
@@ -16,81 +17,99 @@ if (!isProduction) {
     dotenv.config();
 }
 
-// Middleware
-const app = express();
-app.use(compression());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'localhost');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    next();
-});
-if (isProduction) {
-    app.use((req, res, next) => {
-      if (req.header('x-forwarded-proto') !== 'https') {
-          res.redirect(`https://${req.header('host')}${req.url}`)
-      }
-      else {
-          next()
-      }
-  }) 
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
 }
-app.use(bodyParser());
-app.use(bodyParser.json());
-app.use(session({
-    saveUninitialized: true,
-    resave: true,
-    secret: process.env.SESSION_KEY,
-    store: new RedisStore({
-        url: process.env.REDIS_URL
-    }),
-    cookie: {
-        httpOnly: false,
-        maxAge: 2419200000
+else {
+    // Middleware
+    const app = express();
+    app.use(compression());
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', 'localhost');
+        res.header('Access-Control-Allow-Credentials', 'true');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+        next();
+    });
+    if (isProduction) {
+        app.use((req, res, next) => {
+          if (req.header('x-forwarded-proto') !== 'https') {
+              res.redirect(`https://${req.header('host')}${req.url}`)
+          }
+          else {
+              next()
+          }
+      }) 
     }
-}));
-app.use(express.static(__dirname +'./../'));
+    app.use(bodyParser());
+    app.use(bodyParser.json());
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: process.env.SESSION_KEY,
+        store: new RedisStore({
+            url: process.env.REDIS_URL
+        }),
+        cookie: {
+            httpOnly: false,
+            maxAge: 2419200000
+        }
+    }));
+    app.use(express.static(__dirname +'./../'));
 
-// Authentication
-const Auth = require("../conifg/authentication");
-Auth.init(app);
+    // Authentication
+    const Auth = require("../conifg/authentication");
+    Auth.init(app);
 
-// Routes
-app.post("/api/initlogin", (req, res) => {
-    if (!req.isAuthenticated()) {
-        res.json({});
-        return;
-    }
-    UserService.getInitInfo(req.session.passport.user.id)
-    .then(data => {
-        let returnData = data;
-        res.json(returnData);
-    })
-    .catch(err => {
-        console.log(err);
-        return err;
+    // Routes
+    app.post("/api/initlogin", (req, res) => {
+        if (!req.isAuthenticated()) {
+            res.json({});
+            return;
+        }
+        UserService.getInitInfo(req.session.passport.user.id)
+        .then(data => {
+            let returnData = data;
+            res.json(returnData);
+        })
+        .catch(err => {
+            console.log(err);
+            return err;
+        });
+
+    });
+    app.use("/api/signup", require("./routes/SignupRoutes"))
+    app.use("/api/food", require("./routes/LoggingRoutes"))
+    app.use("/api/login", require("./routes/LoginRoutes"));
+    app.use("/api/weight", require("./routes/WeightRoutes"));
+    app.use("/api/workouts", require("./routes/WorkoutRoutes"));
+    app.use("/api/userinfo", require("./routes/UserInfoRoutes"));
+    app.post('/api/rss', (req, res) => {
+        RssFeedService.getFromUrl(req.body.url)
+            .then(data => res.json(data))
+            .catch(err => res.json([]))
+    });
+    app.post("/api/logout", (req, res) => {
+        req.session.destroy(() => {
+            req.logout();
+            res.redirect("/");
+        });
     });
 
-});
-app.use("/api/signup", require("./routes/SignupRoutes"))
-app.use("/api/food", require("./routes/LoggingRoutes"))
-app.use("/api/login", require("./routes/LoginRoutes"));
-app.use("/api/weight", require("./routes/WeightRoutes"));
-app.use("/api/workouts", require("./routes/WorkoutRoutes"));
-app.use("/api/userinfo", require("./routes/UserInfoRoutes"));
-app.post('/api/rss', (req, res) => {
-    RssFeedService.getFromUrl(req.body.url)
-        .then(data => res.json(data))
-        .catch(err => res.json([]))
-});
-app.post("/api/logout", (req, res) => {
-    req.session.destroy(() => {
-        req.logout();
-        res.redirect("/");
-    });
-});
+    app.get('*/**', (req,res) => res.sendFile(path.join(__dirname + "../../index.html")))
+    
+    // Initialize application
+    app.listen(PORT, () => console.log(`Food Tracker is running on port ${PORT}!`));
+}
 
-app.get('*/**', (req,res) => res.sendFile(path.join(__dirname + "../../index.html")))
 
-// Initialize application
-app.listen(PORT, () => console.log(`Food Tracker is running on port ${PORT}!`));
+
+
